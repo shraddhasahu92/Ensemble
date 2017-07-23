e3f4ff2716bce472615db96bc2e80eec1f9b4d47
Shraddha <shraddhasahu92@gmail.com>
2017-07-24 00:52:52 +0530

    Initial commit

---
+++ app/src/main/java/com/listentogether/ensemble/activity/Tmp.java
@@ -0,0 +1,14 @@
+package com.listentogether.ensemble.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Tmp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tmp);
+    }
+}
